e36421b509d720846a61a5a07921f4f3fe036fc9
stefano <stefa.manescotto@gmail.com>
2026-07-15 15:54:53 +0200

    Add Camera Class

---
+++ include/camera.h
@@ -0,0 +1,59 @@
+//
+// Created by stefano on 14/07/26.
+//
+
+#ifndef GFXENGINE_CAMERA_H
+#define GFXENGINE_CAMERA_H
+
+#include <glm/glm.hpp>
+#include <glm/vec3.hpp>
+
+#include "shader.h"
+
+
+class Camera {
+private:
+    float cameraSpeed = 2.5f;
+    float yaw = -90.0;
+    float pitch = 0.0f;
+    float sensitivity = .1f;
+
+    float fov = 45.0f;
+    float aspectW = 800.0f;
+    float aspectH = 600.0f;
+    float nearPlane = 0.1f;
+    float farPlane = 100.0f;
+
+    glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+    glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 projection = glm::mat4(1.0f);
+public:
+    enum cameraMovement {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT,
+    };
+
+    Camera();
+    Camera(float fov, float aspectW, float aspectH);
+    ~Camera();
+
+    void updateMatrices(Shader& shader);
+
+    void setFov(float fov);
+    void setAspect(float aspectW, float aspectH);
+    void setNearPlane(float nearPlane);
+    void setFarPlane(float farPlane);
+    void setSensitivity(float sensitivity);
+
+    float getFov();
+
+    void moveCamera(cameraMovement movementType, float deltaTime);
+    void rotateCamera(float xOffset, float yOffset);
+};
+
+#endif //GFXENGINE_CAMERA_H
---
+++ src/camera.cpp
@@ -0,0 +1,89 @@
+//
+// Created by stefano on 14/07/26.
+//
+
+#include "../include/camera.h"
+
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
+
+Camera::Camera() {}
+
+Camera::Camera(float fov, float aspectW, float aspectH) {
+    this->fov = fov;
+    this->aspectW = aspectW;
+    this->aspectH = aspectH;
+}
+
+Camera::~Camera() {}
+
+void Camera::setFov(float fov) {
+    if (fov >= 1.0f && fov <= 45.0f)
+        this->fov = fov;
+}
+
+void Camera::setAspect(float aspectW, float aspectH) {
+    if (aspectH > .0f && aspectW > .0f) {
+        this->aspectW = aspectW;
+        this->aspectH = aspectH;
+    }
+}
+
+void Camera::setNearPlane(float nearPlane) {
+    if (nearPlane >= 0.1f && nearPlane < farPlane)
+        this->nearPlane = nearPlane;
+}
+
+void Camera::setFarPlane(float farPlane) {
+    if (farPlane > 0.1f && nearPlane < farPlane)
+        this->farPlane = farPlane;
+}
+
+void Camera::setSensitivity(float sensitivity) {
+    if (sensitivity > .0f)
+        this->sensitivity = sensitivity;
+}
+
+float Camera::getFov() {
+    return fov;
+}
+
+void Camera::moveCamera(const cameraMovement movementType, float deltaTime) {
+    const float frameCameraSpeed = cameraSpeed * deltaTime;
+    if (movementType == FORWARD)
+        cameraPos += frameCameraSpeed * cameraFront;
+    if (movementType == BACKWARD)
+        cameraPos -= frameCameraSpeed * cameraFront;
+    if (movementType == LEFT)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * frameCameraSpeed;
+    if (movementType == RIGHT)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * frameCameraSpeed;
+}
+
+void Camera::rotateCamera(float xOffset, float yOffset) {
+    xOffset *= sensitivity;
+    yOffset *= sensitivity;
+
+    yaw += xOffset;
+    pitch += yOffset;
+
+    if (pitch > 89.0f) {
+        pitch = 89.0f;
+    }
+    if(pitch < -89.0f) {
+        pitch = -89.0f;
+    }
+
+    cameraFront.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront.y = sin(glm::radians(pitch));
+    cameraFront.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(cameraFront);
+}
+
+void Camera::updateMatrices(Shader &shader) {
+    view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+    projection = glm::perspective(glm::radians(fov), aspectW / aspectH, nearPlane, farPlane);
+
+    shader.setMat4f("view", view);
+    shader.setMat4f("projection", projection);
+}
